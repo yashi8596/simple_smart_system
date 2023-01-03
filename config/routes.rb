547b089eb23679146d280676a99227702d09985a
yashi8596 <deletio.422shinta@gmail.com>
2023-01-03 14:21:07 +0000

    従業員のログイン機能（未完）の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :public do
-    root "homes#top"
+  scope module: :public do
+    root to: "homes#top"
+    resource :sessions, only:[:new, :create, :destroy]
   end
 
   namespace :admin do
