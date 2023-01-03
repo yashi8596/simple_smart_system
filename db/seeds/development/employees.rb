7e0f799ad12d62ba561baadb2908c012bc2f19a0
yashi8596 <deletio.422shinta@gmail.com>
2023-01-03 13:17:57 +0000

    シードデータの投入

---
+++ db/seeds/development/employees.rb
@@ -0,0 +1,12 @@
+Employee.create!(
+  employee_number: "3807604",
+  last_name: "山口",
+  first_name: "慎太郎",
+  last_name_kana: "ヤマグチ",
+  first_name_kana: "シンタロウ",
+  password: "shintaro",
+  address: "愛知県愛知市愛知町1-2-3",
+  telephone_number: "09012345678",
+  email: "yamaguchi@exam.com",
+  start_date: Date.today
+)
