623db90380c611bcb95921ed2e95de3780dc318e
yashi8596 <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2023-01-03 12:39:21 +0000

    Employeeモデルとテーブルの作成

---
+++ db/schema.rb
@@ -0,0 +1,36 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2023_01_03_113252) do
+
+  create_table "employees", force: :cascade do |t|
+    t.integer "employee_number", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.string "hashed_password", null: false
+    t.string "address", null: false
+    t.integer "telephone_number", null: false
+    t.string "email", null: false
+    t.integer "number_of_paid_leave", default: 0, null: false
+    t.date "start_date", null: false
+    t.date "end_date"
+    t.boolean "suspended", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index "lower(email)", name: "index_employees_on_lower_email", unique: true
+    t.index ["employee_number"], name: "index_employees_on_employee_number", unique: true
+    t.index ["last_name_kana", "first_name_kana"], name: "index_employees_on_last_name_kana_and_first_name_kana"
+  end
+
+end
