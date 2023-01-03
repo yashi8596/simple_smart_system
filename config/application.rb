cec2a9d26254b08f0867c000feb3751ad134b193
yashi8596 <deletio.422shinta@gmail.com>
2023-01-03 11:03:49 +0000

    Initial Commit

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ module Sss
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = "Tokyo"
+    config.i18n.load_path +=
+      Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
