b39b8545da69586600ab8b135da7d58b36241f5c
yashi8596 <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2023-01-03 14:19:58 +0000

    従業員のログイン機能（未完）の作成

--- app/controllers/public/base.rb
+++ app/controllers/public/base.rb
@@ -1,3 +1,15 @@
 class Public::Base < ApplicationController
   layout 'public'
+
+  private
+
+  def current_employee
+    if session[:employee_id]
+
+      @current_employee ||= Employee.find_by(id: session[:employee_id])
+
+    end
+  end
+
+  helper_method :current_employee
 end
---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,10 @@
+class Public::SessionsController < Public::Base
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
