3137367310d8026c2933d8cfce123a679f35642d
yashi8596 <deletio.422shinta@gmail.com>
2023-01-03 11:05:01 +0000

    管理者、従業員のトップページ作成（仮）

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :public do
+    root "homes#top"
+  end
+
+  namespace :admin do
+    root "homes#top"
+  end
 end
