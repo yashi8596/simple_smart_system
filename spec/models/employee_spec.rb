83268b3b1a8ccdab255572a171c70c8e7bd7c2af
yashi8596 <deletio.422shinta@gmail.com>
2023-01-03 13:02:17 +0000

    ハッシュ値のテスト

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe "#password=" do
+    example "文字列を与えると、hashed_passwordは長さ60の文字列になる" do
+      employee = Employee.new
+      employee.password = "dmmwebcamp"
+      expect(employee.hashed_password).to be_kind_of(String)
+      expect(employee.hashed_password.size).to eq(60)
+    end
+  end
+
+  example "nilを与えると、hashed_passwordはnilになる" do
+    employee = Employee.new(hashed_password: "x")
+    employee.password = nil
+    expect(employee.hashed_password).to be_nil
+  end
 end
