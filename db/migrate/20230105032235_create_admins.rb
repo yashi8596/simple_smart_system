92b4412f9ba7ae83a6ea9d42c47deaa4d8bf515d
yashi8596 <deletio.422shinta@gmail.com>
2023-01-05 04:07:14 +0000

    管理者テーブル、モデルの作成

---
+++ db/migrate/20230105032235_create_admins.rb
@@ -0,0 +1,14 @@
+class CreateAdmins < ActiveRecord::Migration[6.1]
+  def change
+    create_table :admins do |t|
+      t.integer :employee_id, null: false
+      t.string :hashed_password, null: false
+      t.string :email, null: false
+      t.boolean :suspended, null: false, default: false
+      t.timestamps
+    end
+
+    add_index :admins, "LOWER(email)", unique: true
+
+  end
+end
