2dbed0be4715f9d0282482fc60ff9d1a205d6d2a
yashi8596 <ec2-user@ip-172-31-12-163.ap-northeast-1.compute.internal>
2023-01-05 04:04:37 +0000

    停止アカウントでログインした時のフラッシュメッセージを追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -15,9 +15,17 @@ class Public::SessionsController < Public::Base
     end
 
     if Public::Authenticator.new(employee).authenticate(@form.password)
-      session[:employee_id] = employee.id
-      flash.notice = "ログインしました。"
-      redirect_to :root
+      if employee.suspended?
+        # suspendedがtrueの場合に表示されるフラッシュメッセージ
+        flash.now.alert = "アカウントが停止されています。"
+        render action: "new"
+
+      else
+        session[:employee_id] = employee.id
+        flash.notice = "ログインしました。"
+        redirect_to :root
+
+      end
     else
       flash.now.alert = "従業員番号またはパスワードが正しくありません。"
       render action: "new"
--- app/services/public/authenticator.rb
+++ app/services/public/authenticator.rb
@@ -6,7 +6,6 @@ class Public::Authenticator
 
   def authenticate(raw_password)
     @employee &&
-      !@employee.suspended? && #無効フラグがfalseか
       @employee.hashed_password &&
       @employee.start_date <= Date.today && #開始日がDate.today以前であるか
       (@employee.end_date.nil? || @employee.end_date > Date.today) && #終了日がnil、またはDate.today以降であるか
