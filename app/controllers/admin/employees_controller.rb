9ff4ca886079e7cbd6df454e02b8ccee4bfea4e6
yashi8596 <deletio.422shinta@gmail.com>
2023-01-05 10:39:15 +0000

    管理者側にemployeeコントローラを作成

---
+++ app/controllers/admin/employees_controller.rb
@@ -0,0 +1,20 @@
+class Admin::EmployeesController < Admin::Base
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     resource :sessions, only:[:new, :create, :destroy]
+    resource :employees, only:[:edit, :update]
+    resources :requests, only:[:new, :create, :show, :update]
+    get "requests/:id/cancel" => "requests#cancel", as: "cancel_request_path" #申請取り消し用
   end
 
   namespace :admin do
     root "homes#top"
     resource :sessions, only:[:new, :create, :destroy]
+    resources :employees, except:[:index]
   end
 end
