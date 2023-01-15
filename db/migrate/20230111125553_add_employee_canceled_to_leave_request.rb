ec156b2e144a501fee10cefb60c0b774493b8a02
yashi8596 <deletio.422shinta@gmail.com>
2023-01-15 09:56:16 +0000

    データベースの不要ファイルの削除

--- db/migrate/20230111125553_add_employee_canceled_to_leave_request.rb
+++
@@ -1,6 +0,0 @@
-class AddEmployeeCanceledToLeaveRequest < ActiveRecord::Migration[6.1]
-  def change
-    add_column :leave_requests, :employee_canceled, :boolean, null: false, default: false
-    remove_column :leave_requests, :reason_for_cancel, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_11_125553) do
+ActiveRecord::Schema.define(version: 2023_01_11_124748) do
 
   create_table "admins", force: :cascade do |t|
+    t.integer "employee_number", null: false
+    t.string "hashed_password", null: false
     t.string "email", null: false
     t.boolean "suspended", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "employee_number", null: false
-    t.string "hashed_password"
     t.index "LOWER(email)", name: "index_admins_on_LOWER_email", unique: true
   end
 
@@ -51,14 +51,14 @@ ActiveRecord::Schema.define(version: 2023_01_11_125553) do
   end
 
   create_table "leave_requests", force: :cascade do |t|
-    t.date "preferred_date", null: false
-    t.text "reason_for_request", default: "0", null: false
-    t.boolean "permitted", default: false, null: false
-    t.boolean "canceled", default: false, null: false
     t.integer "employee_number", null: false
+    t.date "preferred_date", null: false
+    t.text "reason_for_request", null: false
+    t.boolean "permitted", default: true, null: false
+    t.boolean "canceled", default: false, null: false
+    t.boolean "employee_canceled", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "employee_canceled", default: false, null: false
   end
 
   create_table "salaries", force: :cascade do |t|
@@ -67,14 +67,14 @@ ActiveRecord::Schema.define(version: 2023_01_11_125553) do
     t.integer "extra_hour", default: 0, null: false
     t.integer "midnight_hour", default: 0, null: false
     t.integer "employee_number", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.integer "used_paid_leave", default: 0, null: false
     t.integer "total_workday", null: false
     t.integer "absent", default: 0, null: false
     t.float "total_minute", default: 0.0, null: false
     t.float "extra_minute", default: 0.0, null: false
     t.float "midnight_minute", default: 0.0, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "employee_events", "employees"
