e0c0470b84ba4a2cd512b4d0156a720f1e797a31
yashi8596 <deletio.422shinta@gmail.com>
2023-01-23 22:51:31 +0000

    コード修正

--- app/controllers/public/employees_controller.rb
+++ app/controllers/public/employees_controller.rb
@@ -1,7 +1,25 @@
-class Public::EmployeesController < ApplicationController
+class Public::EmployeesController < Public::Base
+  def show
+    @employee = current_employee
+  end
+
   def edit
+    @employee = current_employee
   end
 
   def update
+    @employee = current_employee
+    if @employee.update(employee_params)
+      flash.notice = "従業員情報を更新しました。"
+      redirect_to root_path
+    else
+      flash.now.alert = "お手数ですが、操作をやり直して下さい。"
+      render action: "edit"
+    end
+  end
+
+  private
+  def employee_params
+    params.require(:employee).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email)
   end
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,6 @@
 class Public::HomesController < Public::Base
   def top
     @employee = current_employee
-    @events = EmployeeEvent.all
-    @leave_requests = LeaveRequest.where(employee_number: current_employee).where(permitted: [nil, false])
+    @leave_requests = LeaveRequest.where(employee_number: current_employee)
   end
 end
