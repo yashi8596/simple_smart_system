fd3ff80b8fd544555a27d0d063fa6c7fb5346e25
yashi8596 <deletio.422shinta@gmail.com>
2023-01-23 03:46:21 +0000

    ストロングパラメータのテストファイルの作成

--- spec/requests/admin/employees_management_spec.rb
+++ spec/requests/admin/employees_management_spec.rb
@@ -7,18 +7,35 @@ describe "管理者による従業員管理" do
   describe "新規登録" do
     let(:params_hash) { attributes_for(:employee) }
 
-    example "従業員一覧ページにリダイレクト" do
-      post admin_employees_path, params: { employee: params_hash }
-      expect(response).to redirect_to(admin_root_path)
+    example "従業員詳細ページにリダイレクト" do
+      post admin_employees_url, params: { employee: params_hash }
+      expect(response).to redirect_to(admin_employee_url[:employee])
     end
 
     example "例外ActionController::ParameterMissingが発生" do
-      expect { post admin_employees_path }.to raise_error(ActionController::ParameterMissing)
+      expect { post admin_employees_url }.to raise_error(ActionController::ParameterMissing)
     end
   end
 
   describe "更新" do
     let(:employee) { create(:employee) }
     let(:params_hash) { attributes_for(:employee) }
+
+    example "suspendedフラグをセットする" do
+      params_hash.merge!(suspended: true)
+      patch admin_employee_url(employee),
+        params: { employee: params_hash }
+      employee.reload
+      expect(employee).to be_suspended
+    end
+
+    example "hashed_passwordの値は書き換え不可" do
+      params_hash.delete(:password)
+      params_hash.merge!(hashed_password: "x")
+      expect {
+        patch admin_employee_url(employee),
+          params: { employee: params_hash }
+      }.not_to change { employee.hashed_password.to_s }
+    end
   end
 end
