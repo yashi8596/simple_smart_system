1c583d410a62f5fe5a3591e9c01fa03f00e150f5
yashi8596 <deletio.422shinta@gmail.com>
2023-01-26 03:39:32 +0000

    単数リソースのルート名が複数形だったので単数形に変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope module: :public do
     root to: "homes#top"
-    resource :sessions, only:[:new, :create, :destroy]
-    resource :employees, only:[:edit, :update]
+    resource :session, only:[:new, :create, :destroy]
+    resource :employee, only:[:edit, :update]
     resources :leave_requests, except:[:index, :destroy]
     get "leave_requests/:id/cancel" => "leave_requests#cancel", as: "cancel"
     patch "leave_requests/:id/cancel" => "leave_requests#cancel_update", as: "cancel_update" #申請取り消し用
     get "salaries/:id" => "salaries#show", as: "salary"
+    resource :password, only:[:edit, :update]
   end
 
   namespace :admin do
     root "homes#top"
-    resource :sessions, only:[:new, :create, :destroy]
+    resource :session, only:[:new, :create, :destroy]
     resources :employees, except:[:index]
     get "employees/:id/confirm" => "employees#confirm", as: "confirm" #従業員データ物理削除時の確認用画面
     resources :salaries, except:[:destroy]
