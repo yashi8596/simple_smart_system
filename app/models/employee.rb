03b57eb1021d506834f89a2cbdbf1b666870ed4b
yashi8596 <deletio.422shinta@gmail.com>
2023-01-26 03:40:06 +0000

    バリデーションの追加

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,6 +3,20 @@ class Employee < ApplicationRecord
   has_many :requests, dependent: :destroy
   has_many :employee_events, dependent: :destroy
 
+  KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+
+  validates :last_name, :first_name, :email, :address, :telephone_number, presence: true
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP, allow_blank: true }
+  validates :start_date, presence: true, date: {
+    after_or_equal_to: Date.new(2000, 1, 1),
+    before: -> (obj) { 1.year.from_now.to_date },
+    allow_blank: true
+  }
+  validates :end_date, date: {
+    after: :start_date,
+    before: -> (obj) { 1.year.from_now.to_date },
+    allow_blank: true
+  }
   def password=(raw_password)
     if raw_password.kind_of?(String)
 
