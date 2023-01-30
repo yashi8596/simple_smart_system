0764747acc1c8bc2795a79aa66a38cd42eda98c3
yashi8596 <deletio.422shinta@gmail.com>
2023-01-30 02:34:59 +0000

    パスワードの再設定機能を追加

--- app/controllers/public/passwords_controller.rb
+++ app/controllers/public/passwords_controller.rb
@@ -1,8 +1,22 @@
 class Public::PasswordsController < Public::Base
   def edit
-    
+    @change_password_form = Public::ChangePasswordForm.new(object: current_employee)
   end
-  
+
   def update
+    @change_password_form = Public::ChangePasswordForm.new(employee_params)
+    @change_password_form.object = current_employee
+    if @change_password_form.save
+      flash.notice = "パスワードを変更しました。"
+      redirect_to root_path
+    else
+      flash.now.alert = "入力にに誤りがあります。"
+      render action: "edit"
+    end
+  end
+
+  private
+  def employee_params
+    params.require(:public_change_password_form).permit(:current_password, :new_password, :new_password_confirmation)
   end
 end
