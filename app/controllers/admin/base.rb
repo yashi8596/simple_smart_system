3dbd3642b914d7d4ea46305a0617abdfac215b88
yashi8596 <deletio.422shinta@gmail.com>
2023-01-30 02:15:31 +0000

    有給の削除判定 を行うメソッドを追記

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,15 +1,11 @@
 class Admin::Base < ApplicationController
   before_action :authorize #管理者以外のアクセス制御
+  before_action :check_account
+  before_action :check_timeout
+  before_action :check_lr_destroy
   layout 'admin'
 
   private
-  def authorize
-    unless current_admin
-      flash.alert = "管理者としてログインしてください。"
-      redirect_to new_admin_sessions_path
-    end
-  end
-
   def current_admin
     if session[:admin_id]
       @current_admin ||= Admin.find_by(id: session[:admin_id])
@@ -18,4 +14,45 @@ class Admin::Base < ApplicationController
 
   helper_method :current_admin
 
+  def authorize
+    unless current_admin
+      flash.alert = "管理者としてログインしてください。"
+      redirect_to new_admin_session_path
+    end
+  end
+
+  def check_account
+    if current_admin && current_admin.suspended?
+      session.delete(:admin_id)
+      flash.alert = "アカウントが無効になりました。"
+      redirect_to new_admin_session_path
+    end
+  end
+
+  #セッションタイムアウトの設定時間
+  TIMEOUT = 60.minutes
+
+  def check_timeout
+    if current_admin
+      if session[:admin_last_access_time] >= TIMEOUT.ago
+        session[:admin_last_access_time] = Time.current
+      else
+        session.delete(:admin_id)
+        flash.alert = "セッションタイムアウトになりました。再度ログインして下さい。"
+        redirect_to new_admin_session_path
+      end
+    end
+  end
+
+  #有給の自動削除
+  def check_lr_destroy
+    @leave_requests = LeaveRequest.all
+    if @leave_requests
+      @leave_requests.each do |lr|
+        if lr.auto_destroy
+          lr.destroy
+        end
+      end
+    end
+  end
 end
--- app/models/leave_request.rb
+++ app/models/leave_request.rb
@@ -1,3 +1,18 @@
 class LeaveRequest < ApplicationRecord
-  belongs_to :employee, foreign_key: 'employee_number', primary_key: 'employee_number'
+  belongs_to :employee
+
+  def emp_request
+    #申請(取消も含む)可能かどうかの判定メソッド（取得予定日時の10日前まで(従業員用)）
+    preferred_date.ago(10.days) >= Date.current
+  end
+
+  def ad_request
+    #申請(取消も含む)可能かどうかの判定メソッド（取得予定日時の7日前まで(管理者用)）
+    preferred_date.ago(7.days) >= Date.current
+  end
+
+  def auto_destroy
+    #有給の自動削除判定メソッド
+    preferred_date <= Date.today
+  end
 end
