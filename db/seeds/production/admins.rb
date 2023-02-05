290f83898b09a81c835373224f372208708cd8c2
yashi8596 <deletio.422shinta@gmail.com>
2023-02-05 17:24:05 +0000

    本番環境用のテストデータのディレクトリの作成

---
+++ db/seeds/production/admins.rb
@@ -0,0 +1,5 @@
+Admin.create!(
+  employee_number: "3807604",
+  password: "shintaro",
+  email: "yamaguchi@exam.com",
+)
---
+++ db/seeds/production/employees.rb
@@ -0,0 +1,47 @@
+last_names = %w{
+  佐藤:サトウ:sato
+  山本:ヤマモト:yamamoto
+  上村:ウエムラ:uemura
+  前川:マエカワ:maekawa
+}
+
+first_names = %w{
+  翔太:ショウタ:shouta
+  一:ハジメ:hajime
+  遥:ハルカ:haruka
+  夏帆:カホ:kaho
+  恵梨香:エリカ:erika
+}
+
+addresses = %w{
+  愛知:ABC:&&&:1-2
+  岐阜:DEF:###:3-5
+  長野:GHI:$$$:1-4
+  静岡:JKL:!!!:2-1
+  三重:MNO:???:2-2
+  富山:PQR:+++:4-5
+  石川:STU:===:3-2
+  福井:VWX:@@@:4-1
+}
+
+20.times do |n|
+  ln = last_names[n % 4].split(":")
+  fn = first_names[n % 5].split(":")
+  ad = addresses[n % 8].split(":")
+  tn = 8.times.map { rand(9) }.join
+
+  Employee.create!(
+    employee_number: 7.times.map { rand(8) }.join,
+    last_name: ln[0],
+    first_name: fn[0],
+    last_name_kana: ln[1],
+    first_name_kana: fn[1],
+    password: "password",
+    address: "#{ad[0]}県#{ad[1]}市#{ad[2]}町#{ad[3]}",
+    telephone_number: "090#{tn}",
+    email: "#{ln[2]}.#{fn[2]}@exam.com",
+    start_date: (100 - n).days.ago.to_date,
+    end_date: n == 0 ?  Date.today : nil,
+    suspended: n == 1
+  )
+end
