1da71593417375b7de5917e1a30a8dc91a7120e4
yashi8596 <deletio.422shinta@gmail.com>
2023-02-05 18:09:29 +0000

    コード修正

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,6 @@
 class Admin::HomesController < Admin::Base
   def top
-    @employees = Employee.order(id: :desc)
+    @employees = Employee.order(employee_number: :desc)
     @employees = @employees.page(params[:page]).per(10)
   end
 end
