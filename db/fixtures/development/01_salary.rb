9c339400baf5850e4b779602e9ee2b6890ea4c9e
yashi8596 <deletio.422shinta@gmail.com>
2023-05-14 17:02:30 +0000

    勤怠管理機能用のシードデータの投入

---
+++ db/fixtures/development/01_salary.rb
@@ -0,0 +1,14 @@
+Salary.seed do |s|
+  s.id = 1
+  s.employee_id = "0000101"
+  s.wage = 1500
+  s.total_hour = 0
+  s.extra_hour = 0
+  s.absent_hour = 0
+  s.total_minute = 0.0
+  s.extra_minute = 0.0
+  s.absent_minute = 0.0
+  s.absent_days = 0
+  s.run_days = 0
+  s.used_paid_leave = 0
+end
---
+++ db/fixtures/development/02_time_record.rb
@@ -0,0 +1,129 @@
+TimeRecord.seed do |s|
+  s.id = 1
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 1)
+  s.started_at = DateTime.new(2023, 4, 30, 22, 41, 0)
+  s.finished_at = DateTime.new(2023, 5, 1, 9, 1, 0)
+  s.total_time = 9.75
+  s.absent_time = 0.0
+  s.extra_time = 1.75
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 2
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 2)
+  s.started_at = DateTime.new(2023, 5, 1, 22, 58, 0)
+  s.finished_at = DateTime.new(2023, 5, 2, 10, 31, 0)
+  s.total_time = 10.0
+  s.absent_time = 0.0
+  s.extra_time = 2.0
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 3
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 3)
+  s.started_at = DateTime.new(2023, 5, 2, 22, 37, 0)
+  s.finished_at = DateTime.new(2023, 5, 3, 10, 33, 0)
+  s.total_time = 10.0
+  s.absent_time = 0.0
+  s.extra_time = 2.0
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 4
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 4)
+  s.started_at = DateTime.new(2023, 5, 3, 22, 47, 0)
+  s.finished_at = DateTime.new(2023, 5, 4, 10, 30, 0)
+  s.total_time = 10.0
+  s.absent_time = 0.0
+  s.extra_time = 2.0
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 5
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 5)
+  s.started_at = DateTime.new(2023, 5, 4, 22, 45, 0)
+  s.finished_at = DateTime.new(2023, 5, 5, 10, 31, 0)
+  s.total_time = 10.0
+  s.absent_time = 0.0
+  s.extra_time = 2.0
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 6
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 8)
+  s.started_at = DateTime.new(2023, 5, 7, 23, 12, 0)
+  s.finished_at = DateTime.new(2023, 5, 8, 10, 33, 0)
+  s.total_time = 9.75
+  s.absent_time = 0.25
+  s.extra_time = 1.75
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 7
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 9)
+  s.started_at = DateTime.new(2023, 5, 8, 22, 48, 0)
+  s.finished_at = DateTime.new(2023, 5, 9, 10, 30, 0)
+  s.total_time = 10.0
+  s.absent_time = 0.0
+  s.extra_time = 2.0
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 8
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 10)
+  s.started_at = DateTime.new(2023, 5, 9, 22, 40, 0)
+  s.finished_at = DateTime.new(2023, 5, 10, 8, 45, 0)
+  s.total_time = 8.5
+  s.absent_time = 0.0
+  s.extra_time = 0.5
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 9
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 11)
+  s.started_at = DateTime.new(2023, 5, 10, 22, 52, 0)
+  s.finished_at = DateTime.new(2023, 5, 11, 10, 32, 0)
+  s.total_time = 10.0
+  s.absent_time = 0.0
+  s.extra_time = 2.0
+  s.division = 1
+end
+
+TimeRecord.seed do |s|
+  s.id = 10
+  s.employee_id = "0000101"
+  s.salary_id = 1
+  s.work_date = Date.new(2023, 5, 12)
+  s.started_at = DateTime.new(2023, 5, 11, 22, 38, 0)
+  s.finished_at = DateTime.new(2023, 5, 12, 10, 00, 0)
+  s.total_time = 9.75
+  s.absent_time = 0.0
+  s.extra_time = 1.75
+  s.division = 1
+end
